141704203b2dd9548145a62e53591e390cc41ee4
Your Name <JulienneJulien15@gmail.com>
2023-02-04 21:56:42 -0500

    Added PORT to test home route

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const inquirer = require('inquirer');
 require('console.table');
 const mysql = require('mysql2');
 
+const PORT = process.env.PORT || 8000;
+app.get("/", function (req, res) {
+    res.send("HEY TESTING ROUTE");
+});
+
+app.listen(PORT, () => console.log(' Server Listening on port ' + PORT));
 // EXPRESS MIDDLEWARE
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
