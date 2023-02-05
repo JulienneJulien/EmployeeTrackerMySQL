79be3fa11a29ea0b5d4721710f829518efff980d
Your Name <JulienneJulien15@gmail.com>
2023-02-04 22:29:08 -0500

    created mysql connection file

---
+++ db/sqlconnection.js
@@ -0,0 +1,15 @@
+const connection = require("./sqlconnection")
+// TO CONNECT TO SQL DATABASE
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // MySQL password
+      password: 'Pass',
+      database: 'Business_db'
+    },
+    console.log(`Connected to the Business_db database.`)
+  );
+
+  module.exports = connection;
