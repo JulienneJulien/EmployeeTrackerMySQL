bedef0793db37e382185975f6f9fc4d7d1f573be
Your Name <JulienneJulien15@gmail.com>
2023-02-04 22:57:45 -0500

    added table layout

---
+++ db/schema.sql
@@ -0,0 +1,30 @@
+-- DATABASE
+DROP DATABASE IF EXISTS Business_db;
+CREATE DATABASE Business_db;
+USE Business_db;
+-- DEPARTMENT 
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    name VARCHAR(30),
+    PRIMARY KEY (id)
+);
+-- ROLE 
+CREATE TABLE role (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30),
+    salary DECIMAL(10.2),
+    department_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+-- EMPLOYEE 
+CREATE TABLE department (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
